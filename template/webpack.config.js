cdabcb9bdcc359077630fe4ad36039b6cfe27282
Matteo Nicoletti <matteo@kaosmos.it>
2018-01-31 16:30:08 +0100

    webpack.js and package.json

--- template/webpack.config.js
+++ template/webpack.config.js
@@ -2,14 +2,65 @@ const path = require('path');
 const ExtractTextPlugin = require("extract-text-webpack-plugin");
 const autoprefixer = require("autoprefixer");
 
+const SpriteLoaderPlugin = require('svg-sprite-loader/plugin'); // SVG icons system
+
 const extractSass = new ExtractTextPlugin({
   filename: "stylesheets/[name].css",
   disable: process.env.NODE_ENV === "development"
 });
 
+// SVG icons system prefs - start
+var svgSpriteLoaderConfig = JSON.stringify({
+  extract: true,
+  spriteFilename: "sprite.svg"
+});
+var svgoConfig = JSON.stringify({
+  multipass: true,
+  pretty: true,
+  plugins: [
+    {cleanupAttrs: true},
+    {cleanupEnableBackground: true},
+    {cleanupIDs: true},
+    {cleanupListOfValues: true},
+    {cleanupNumericValues: true},
+    {collapseGroups: true},
+    {convertColors: true},
+    {convertPathData: true},
+    {convertShapeToPath: true},
+    {convertStyleToAttrs: true},
+    {convertTransform: true},
+    {mergePaths: true},
+    {moveElemsAttrsToGroup: true},
+    {moveGroupAttrsToElems: true},
+    {removeAttrs: {attrs: '(fill|stroke)'}}, // if you don't want any color from the original svg
+    {removeComments: true},
+    {removeDesc: false}, // for usability reasons
+    {removeDimensions: true},
+    {removeDoctype: true},
+    {removeEditorsNSData: true},
+    {removeEmptyAttrs: true},
+    {removeEmptyContainers: true},
+    {removeEmptyText: true},
+    {removeHiddenElems: true},
+    {removeMetadata: true},
+    {removeNonInheritableGroupAttrs: true},
+    {removeRasterImages: true}, // bitmap! you shall not pass!
+    {removeTitle: false}, // for usability reasons
+    {removeUnknownsAndDefaults: true},
+    {removeUnusedNS: true},
+    {removeUselessDefs: true},
+    {removeUselessStrokeAndFill: true},
+    {removeViewBox: false},
+    {removeXMLProcInst: true},
+    {sortAttrs: true}
+  ]
+});
+// SVG icons system prefs - end
+
 module.exports = {
   entry: {
     application: './source/javascripts/index.js',
+    svg: './source/svg/svg_icons.js',
     styles: './source/stylesheets/application.sass'
   },
   resolve: {
@@ -61,8 +112,18 @@ module.exports = {
           ],
           fallback: "style-loader"
         })
+      },
+      {
+        test: /\.svg$/,
+        loaders: [
+          'svg-sprite-loader?' + svgSpriteLoaderConfig,
+          'svgo?' + svgoConfig
+        ]
       }
     ]
   },
-  plugins: [ extractSass ]
+  plugins: [
+    extractSass,
+    new SpriteLoaderPlugin({ plainSprite: true }) // renders a plain sprite without styles and usages in extract mode, as we want it
+  ]
 };
